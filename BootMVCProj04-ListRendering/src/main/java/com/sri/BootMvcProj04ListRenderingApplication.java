523bb205d6a371656b092141d7a7a8d0c8955e12
Sritam369 <chakrabarttysritam@gmail.com>
2026-05-05 18:19:46 +0530

    metadata

---
+++ BootMVCProj04-ListRendering/src/main/java/com/sri/BootMvcProj04ListRenderingApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj04ListRenderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj04ListRenderingApplication.class, args);
+	}
+
+}
