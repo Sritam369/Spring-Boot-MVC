b536db23176ab087ec3be308ae9002a2e2456b00
Sritam369 <chakrabarttysritam@gmail.com>
2026-05-09 10:05:29 +0530

    metadata

---
+++ BootMVCProj06-FlightBooking-Exam/src/main/java/com/sri/BootMvcProj06FlightBookingExamApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj06FlightBookingExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj06FlightBookingExamApplication.class, args);
+	}
+
+}
