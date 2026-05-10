9e9caef499ca3fc99f73dd2abce269e36001451c
Sritam369 <chakrabarttysritam@gmail.com>
2026-05-10 18:49:20 +0530

    metadata

---
+++ BootMVCProj08-FormDataBinding/src/main/java/com/sri/BootMvcProj08FormDataBindingApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj08FormDataBindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj08FormDataBindingApplication.class, args);
+	}
+
+}
