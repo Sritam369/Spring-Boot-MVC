7290ed801780a49a6e01f3ec9d01d6855600e373
Sritam369 <chakrabarttysritam@gmail.com>
2026-05-11 19:00:50 +0530

    metadata

---
+++ BootMVCProj09-TwoWayFormDataBinding/src/main/java/com/sri/BootMvcProj09TwoWayFormDataBindingApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj09TwoWayFormDataBindingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj09TwoWayFormDataBindingApplication.class, args);
+	}
+
+}
