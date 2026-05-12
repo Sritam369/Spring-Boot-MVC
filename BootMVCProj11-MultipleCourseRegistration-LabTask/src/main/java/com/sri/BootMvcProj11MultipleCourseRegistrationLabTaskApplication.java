b74fe3a93c658bfc35c953ecaf583977a2e64a2f
Sritam369 <chakrabarttysritam@gmail.com>
2026-05-12 19:55:55 +0530

    Data binding with multiple values taking as list of requestparam

---
+++ BootMVCProj11-MultipleCourseRegistration-LabTask/src/main/java/com/sri/BootMvcProj11MultipleCourseRegistrationLabTaskApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMvcProj11MultipleCourseRegistrationLabTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMvcProj11MultipleCourseRegistrationLabTaskApplication.class, args);
+	}
+
+}
