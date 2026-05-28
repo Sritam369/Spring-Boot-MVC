8557dacffc054029dc63a7b159fcc773c4597a1d
Sritam369 <chakrabarttysritam@gmail.com>
2026-05-28 19:18:13 +0530

    Sending data as JSON

---
+++ BootRestProj04-DataBindingInRestAPI/src/main/java/com/sri/BootRestProj04DataBindingInRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRestProj04DataBindingInRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootRestProj04DataBindingInRestApiApplication.class, args);
+	}
+
+}
