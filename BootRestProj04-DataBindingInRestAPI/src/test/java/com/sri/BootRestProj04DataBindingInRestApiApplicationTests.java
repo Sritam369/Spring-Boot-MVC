7fd9765e2bf857289c8f99d0c297abfdceee2e18
Sritam369 <chakrabarttysritam@gmail.com>
2026-05-28 19:18:13 +0530

    Sending data as json

---
+++ BootRestProj04-DataBindingInRestAPI/src/test/java/com/sri/BootRestProj04DataBindingInRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootRestProj04DataBindingInRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
