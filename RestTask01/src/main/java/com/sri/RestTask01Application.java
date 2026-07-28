827d610fa2d2622f8bef9691512c6a0d2326d624
Sritam369 <chakrabarttysritam@gmail.com>
2026-07-28 11:22:58 +0530

    Rest API Task

---
+++ RestTask01/src/main/java/com/sri/RestTask01Application.java
@@ -0,0 +1,13 @@
+package com.sri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTask01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTask01Application.class, args);
+	}
+
+}
